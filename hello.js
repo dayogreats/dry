574fe7c16eb9019f53ea41357ebfa77a31987d32
Dayo Greats <dayogreatonline@gmail.com>
2016-12-29 21:54:20 +0100

    files pruned

--- hello.js
+++
@@ -1,5 +0,0 @@
-  // Se ts the HTTP response Content-Disposition header field to “attachment”
-    res.attachment('res.attachment([filename])');
-    // Content-Disposition: attachment; filename="logo.png"
-    // Content-Type: image/png
-  ;
