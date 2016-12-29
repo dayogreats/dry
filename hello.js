3e42c99376d664fd4e11d4afcf6154b61a6ec6df
Dayo Greats <dayogreatonline@gmail.com>
2016-12-29 20:24:41 +0100

    screenshots folder added

--- hello.js
+++ hello.js
@@ -1,3 +1,18 @@
-   router.get('url', function(req, res){
-        res.send('hello world');
-      });
+// add dependencies
+var express = require('express');
+var bodyparser = require('body-parser');
+
+//database
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/myappdatabase');
+
+//middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//intialize express app
+var app = express();
+
+//server listening
+app.listen(3000);
+ 
