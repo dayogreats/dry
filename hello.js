27fd282f6c8999df452df7db115dfdbc5f272631
Dayo Greats <dayogreatonline@gmail.com>
2016-12-29 20:49:52 +0100

    screenshot  added

--- hello.js
+++ hello.js
@@ -1,18 +1,5 @@
-// add dependencies
-var express = require('express');
-var bodyparser = require('body-parser');
-
-//database
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/myappdatabase');
-
-//middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-//intialize express app
-var app = express();
-
-//server listening
-app.listen(3000);
- 
+  // Se ts the HTTP response Content-Disposition header field to “attachment”
+    res.attachment('res.attachment([filename])');
+    // Content-Disposition: attachment; filename="logo.png"
+    // Content-Type: image/png
+  ;
